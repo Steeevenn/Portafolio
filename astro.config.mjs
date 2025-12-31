d8c2c2e8945e79a565f70ce667a214c1b2be78d5
Michael Motivar <michaelbeltran80@gmail.com>
2025-12-30 21:37:33 -0500

    se cambia a protocolo http para poder subirlo a produccion sin necesidad de ceertifcado SSL

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import solidJs from "@astrojs/solid-js";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://mmotivar.dev",
+  site: "http://mmotivar.dev",
   integrations: [
     sitemap({
       changefreq: "yearly",
